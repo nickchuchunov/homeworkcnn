e718f734fb74dbc72840772ef452e7a4415153e5
nickchuchunov <nick.chuchunov@yandex.u>
2021-02-13 19:20:15 +0300

    Знакомство с С# урок 1

---
+++ IntroductionClevel1Chuchunov1.1/IntroductionClevel_1Chuchunov/IntroductionClevel_1Chuchunov/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntroductionClevel_1Chuchunov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Николай";
+            DateTime timecnn = new DateTime(2021, 2, 11);
+            Console.WriteLine("введите ваше имя");
+
+            string name2 = Console.ReadLine();
+            if (name2.Length != 0) { Console.WriteLine("Привет: " + name2 + " сегодня " + timecnn); }
+            else { Console.WriteLine("Привет: " + name1 + " сегодня " + timecnn); }
+        }
+    }
+}
