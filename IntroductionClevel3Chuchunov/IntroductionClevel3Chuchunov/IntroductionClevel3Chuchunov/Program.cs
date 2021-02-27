ab3e0f933051c1c9fd6543f025b0b2832966d3fe
nickchuchunov <nick.chuchunov@yandex.u>
2021-02-27 23:29:15 +0300

    Слияние ветки мастер и level4

---
+++ IntroductionClevel3Chuchunov/IntroductionClevel3Chuchunov/IntroductionClevel3Chuchunov/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace IntroductionClevel3Chuchunov
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            /* 1.Написать программу, выводящую элементы двумерного массива по диагонали.*/
+             int[][] Diagonal = new int[3][];
+             Diagonal[0] = new int[] {1, 2, 3 };
+             Diagonal[1] = new int[] { 4, 5, 6 };
+             Diagonal[2] = new int[] { 7, 8, 9 };
+             for (int i = 0; i < Diagonal.GetLength(0); i++) { for (int j = 0; j < Diagonal.GetLength(0); j++) { if (i == j) { Console.WriteLine(Diagonal[i][j]); } } }; 
+
+
+
+
+            /*2.Написать программу «Телефонный справочник»: создать двумерный массив 5х2, хранящий 
+             * список телефонных контактов: первый элемент хранит имя контакта, второй — номер телефона/ email. */
+             object[][] PhoneDirectory = new object[5][];
+            PhoneDirectory[0] = new object[] {"Андрей", 89994706357};
+            PhoneDirectory[1] = new object[] { "Валентин", 89994706356 };
+            PhoneDirectory[2] = new object[] { "Елена", 89994706355 };
+            PhoneDirectory[3] = new object[] { "Станислав", 89994706354 };
+            PhoneDirectory[4] = new object[] { "Юрий", 89994706353 }; 
+
+            for (int i = 0; i < PhoneDirectory.GetLength(0); i++)
+            { for (int j = 0; j <= 1; j++) { Console.WriteLine("имя {0} Номер телефона {1}", PhoneDirectory[i][0], PhoneDirectory[i][1]); } } 
+
+
+
+            /* 3.Написать программу, выводящую введённую пользователем строку в обратном порядке(olleH вместо Hello). */
+
+            Console.WriteLine("введите текст для преобразования в обратный порядок");
+
+            string s = Console.ReadLine();
+             byte[] s2 = System.Text.Encoding.Default.GetBytes(s);
+             Array.Reverse(s2);
+            string s3 = System.Text.Encoding.Default.GetString(s2);
+            Console.WriteLine(s3);
+
+
+
+
+            /*Задание Номер 3 . 2-й способ вывода строки в обраьном порядке */
+            Console.WriteLine("Вывод текста в обратном порядке способ №2");
+            Console.WriteLine("введите текст для преобразования в обратный порядок");
+
+            string m = Console.ReadLine();
+            byte[] m2 = System.Text.Encoding.Default.GetBytes(m);
+
+
+
+
+            byte[] m4 = new Byte[m2.Length];
+
+            int q = 0;
+            for (int i = m2.Length - 1; i >= 0; i--)
+            { q = q + 1; m4[q - 1] = m2[i]; };
+            string m3 = System.Text.Encoding.Default.GetString(m4);
+            Console.WriteLine(m3);
+
+
+
+        }
+    }
+}
---
+++ IntroductionClevel4Chuchunov/IntroductionClevel4Chuchunov/IntroductionClevel4Chuchunov/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+namespace IntroductionClevel4Chuchunov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*1. Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО в разных аргументах и 
+             * возвращающий объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО. */
+
+
+
+
+            /*static string GetFullName (string lastName, string firstName,  string patronymic) {  string LFP = lastName + " " + firstName + " " + patronymic; return LFP;  };
+
+             string firstNam1 = "Николай";
+             string lastName1 = "Чучунов";
+             string patronymic1 = "Николаевич";
+
+             string a = GetFullName(lastName1, firstNam1, patronymic1);
+             Console.WriteLine(a);*/
+
+
+
+
+            /*2. Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и возвращающую 
+             * число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат на экран. */
+
+            /*  Console.WriteLine("введите числа разделенные пробелом");
+              string b = Console.ReadLine();
+              char[] b1 = b.ToCharArray();
+              char ds = Convert.ToChar(" ");
+              double[] gr1 = new double[b1.Length];
+              int j = 0;
+              for (int i = 0; i < b1.Length; i++) { if (b1[i] != ds) { j++; gr1[j] = Char.GetNumericValue(b1[i]); }; };
+              double gr3 = 0;
+
+
+              foreach (double gr2 in gr1) { gr3 += gr2; };
+              Console.WriteLine(gr3); */
+
+
+
+
+
+
+
+            /*3. Написать метод по определению времени года. На вход подаётся число – порядковый номер месяца.
+             * На выходе — значение из перечисления (enum) — Winter, Spring, Summer, Autumn. Написать метод, принимающий на вход значение из этого перечисления и возвращающий название времени года (зима, весна, лето, осень). Используя эти методы, ввести с клавиатуры номер месяца и вывести название времени года. Если введено некорректное число, вывести в консоль текст «Ошибка: введите число от 1 до 12». */
+            
+            public int seasonyear;
+        enum seasonyear1 : int { Winter = 1, Spring = 2, Summer = 3, Autumn = 4 };
+
+        static int SeasonYear(int month, out int seasonyear)
+        {
+            return seasonyear=0;
+            switch (month)
+            {
+                case 1: seasonyear = (int)seasonyear1.Winter; break;
+                case 2: seasonyear = (int)seasonyear1.Winter; break;
+                case 3: seasonyear = (int)seasonyear1.Spring; break;
+                case 4: seasonyear = (int)seasonyear1.Spring; break;
+                case 5: seasonyear = (int)seasonyear1.Spring; break;
+                case 6: seasonyear = (int)seasonyear1.Summer; break;
+                case 7: seasonyear = (int)seasonyear1.Summer; break;
+                case 8: seasonyear = (int)seasonyear1.Summer; break;
+                case 9: seasonyear = (int)seasonyear1.Autumn; break;
+                case 10: seasonyear = (int)seasonyear1.Autumn; break;
+                case 11: seasonyear = (int)seasonyear1.Autumn; break;
+                case 12: seasonyear = (int)seasonyear1.Winter; break;
+
+            };
+
+            Console.WriteLine("Укажите номер месяца");
+            int moth1 = Convert.ToInt32(Console.ReadLine());
+
+            SeasonYear(moth1, out int seasonyear5);
+            Console.WriteLine("месяц с номером [0] пренадлежит декаде [1]", moth1, seasonyear5);
+        }
+
+
+
+
+
+
+
+    };
+
+
+    };
+
+
+
+
+} 
+    
+
