271a1626af4bbad2433bfc5f2362bd481efb3560
nickchuchunov <nick.chuchunov@yandex.u>
2021-03-09 19:36:19 +0300

    6 практическое задание

---
+++ IntroductionClevel6Chuchunov/IntroductionClevel6Chuchunov/IntroductionClevel6Chuchunov/Array4х4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IntroductionClevel6Chuchunov
+{
+    public class Array4х4
+    {
+       public string[,] arreyString4х4 = new string[4, 4];
+
+        public int[,] arreyInt4x4 = new int[4, 4];
+       
+        
+
+
+        public static int sumarrey;
+
+        
+            
+
+public Array4х4( string[,] arreyString4х4, out int sumarrey  ) 
+        {
+            bool gr = arreyString4х4.GetLength(0) > 4;
+            if (gr = true) { Console.WriteLine("ващ массив больше чем 4х4"); throw new RankException(); };
+
+           
+
+
+            
+
+            sumarrey = 0;
+           
+            int[,] arreyInt4x4 = new int[4, 4];
+            for (int i=0; i< arreyString4х4.GetLength(0);  i++) { for (int j = 0; j < arreyString4х4.GetLength(0); j++) { try { arreyInt4x4[i, j] = Convert.ToInt32(arreyString4х4[i, j]); } catch (FormatException) { Console.WriteLine("в строке {0} и в столбце {1} неправильные данные", i, j); } }; };
+
+            for (int i = 0; i < arreyInt4x4.GetLength(0); i++) { for (int j = 0; j < arreyInt4x4.GetLength(0); j++) {  sumarrey += arreyInt4x4[i, j];  }; };
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ IntroductionClevel6Chuchunov/IntroductionClevel6Chuchunov/IntroductionClevel6Chuchunov/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroductionClevel6Chuchunov
+{
+    class Employee
+    {
+        public string LastFirstPatronymic { get; set; } // фамилия имя отчество
+        public string Post { get; set; } // Должность
+        public string Email { get; set; } // почта
+        public string Pfone { get; set; } // телефон
+        public string Salary { get; set; } // зарплата
+        public int Age { get; set; } // возвраст
+        public Employee[][] argEmployee = new Employee[5][];
+
+
+
+        public Employee(string[][] arreyEmployee ) {
+
+           
+
+            
+
+
+
+
+
+            
+
+            for (int k = 0; k < arreyEmployee.GetLength(0); k++) {
+                  LastFirstPatronymic =arreyEmployee[k][0];
+                   Post = arreyEmployee[k] [1];
+                   Email = arreyEmployee[k] [2];
+                   Pfone = arreyEmployee[k] [3];
+                   Salary = arreyEmployee[k] [4];
+                   Age= Convert.ToInt32(arreyEmployee[k] [5]);
+
+
+                if (Age >= 40) { Console.WriteLine(" фамилия имя отчество{0} Должность {1} почта {2} телефон {3} зарплата {4} возвраст {5} ", LastFirstPatronymic, Post, Email, Pfone, Salary, Age); };
+
+
+
+
+                } 
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
